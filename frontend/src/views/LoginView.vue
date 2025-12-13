35e8efd4cb2cb7a66c040e79c1e403369a98e963
CalmXin <skyqian@88.com>
2025-12-13 15:06:32 +0800

    feat(login): 实现登录页面重定向功能
    - 新增 LoginView 组件用于处理登录逻辑
    - 配置路由重定向至认证服务登录页
    - 移除 App.vue 中的默认模板内容
    - 添加 router-view 以支持路由渲染

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script setup>
+
+import {appRouter} from "@/utils/router.js";
+import CONFIG from "@/config.js";
+
+appRouter.redirect(`${CONFIG.apiBaseUrl}/auth/login`)
+</script>
+
+<style scoped>
+
+</style>
